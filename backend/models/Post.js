100c08b342477d4d48a4ae8543a101ee2b0d03d7
Naik Mubashir <naikmubashir095@gmail.com>
2025-02-02 01:41:55 +0530

    Add user and post models

---
+++ backend/models/Post.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please add a title'],
+    trim: true,
+    maxlength: [100, 'Title cannot exceed 100 characters']
+  },
+  content: {
+    type: String,
+    required: [true, 'Please add content'],
+    maxlength: [5000, 'Content cannot exceed 5000 characters']
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Post', postSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    // select: false
+  }
+},{
+    timestamps: true,
+}
+);
+
+export default mongoose.model('User', userSchema);
