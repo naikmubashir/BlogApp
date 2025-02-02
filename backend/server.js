852e8e5b57377faef745fc873d72d0715dfe3422
Naik Mubashir <naikmubashir095@gmail.com>
2025-02-02 16:59:22 +0530

    Update files

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "https://blog-app-rust-three.vercel.app", // Replace with your frontend URL
   credentials: true, // Allow cookies
 }));
 // Routes
